3ae984b0dd89b9c1a8c76acaa60c773126a66db7
jenny <anthonyjennifer76@gmail.com>
2026-05-19 19:17:39 +0100

    Clean up stray files from repo

--- check_all_pb_headers.py
+++
@@ -1,9 +0,0 @@
-import os
-
-folder = "/Users/jenniferobiekea/.gemini/antigravity/conversations/"
-for filename in os.listdir(folder):
-    if filename.endswith(".pb"):
-        path = os.path.join(folder, filename)
-        with open(path, 'rb') as f:
-            header = f.read(10)
-        print(filename, list(header))
